32b6993c8675a4b0a74d04a77242fcdcb474886b
Diana Nazarova <alllleksandrova@mail.ru>
2023-06-29 19:49:31 +0400

    fix: initial setup

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<div className="footer__top">
+				<p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer;
---
+++ src/components/SavedMovies/SavedMovies.js
@@ -0,0 +1,8 @@
+function SavedMovies() {
+  return (
+    <div>
+    </div>
+  )
+};
+
+export default SavedMovies;
