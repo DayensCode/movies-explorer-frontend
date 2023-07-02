0beb8604e5647757af6b7fa41cff590605aab522
Diana Nazarova <alllleksandrova@mail.ru>
2023-07-02 18:54:08 +0400

    feature: promo, navtab and about-project without adaptive

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,14 @@
+import Promo from './Promo/Promo';
+import NavTab from './NavTab/NavTab';
+import AboutProject from './AboutProject/AboutProject';
+
 function Main() {
   return (
-    <div>
-    </div>
+    <section className="main">
+      <Promo />
+      <NavTab />
+      <AboutProject />
+    </section>
   )
 };
 
