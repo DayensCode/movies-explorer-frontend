f5a1131b8d97e8d75db72ffccec024026e356ae5
Diana Nazarova <alllleksandrova@mail.ru>
2023-07-02 12:17:16 +0400

    feature: footer without adaptive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,17 @@ function Footer() {
 			<div className="footer__top">
 				<p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
 			</div>
+      <div className="footer__bottom">
+        <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
+        <ul className="footer__list">
+          <li className="footer__list-item">
+            <a className="footer__link" href="https://practicum.yandex.ru/" target="_blank" rel="noopener noreferrer">Яндекс.Практикум</a>
+          </li>
+          <li className="footer__list-item">
+            <a className="footer__link" href="https://github.com/DayensCode" target="_blank" rel="noopener noreferrer">Github</a>
+          </li>
+        </ul>
+      </div>
 		</footer>
 	)
 }
