87a2ef23d1bf4336df1e33774f70dff38c636dd7
Diana Nazarova <alllleksandrova@mail.ru>
2023-07-07 18:38:17 +0400

    feature: add logic of delete-save card

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,17 +1,39 @@
-import './MoviesCard.css';
-import { convertingDuration } from '../../utils/utils';
+import { useLocation } from "react-router-dom";
+import "./MoviesCard.css";
+import MovieCardButton from "./MovieCardButton/MovieCardButton";
+import { convertingDuration } from "../../utils/utils";
 
 function MoviesCard({ movieData }) {
-	return (
-		<li className="movie-card">
-			<img className="movie-card__image" alt={movieData.nameRu} src={movieData.image}/>
-			<button className="movie-card__save" type="button">Сохранить</button>
-			<div className="movie-card__description">
-				<p className="movie-card__name">{movieData.nameRU}</p>
-				<span className="movie-card__duration">{convertingDuration(movieData.duration)}</span>
-			</div>
-		</li>
-	)
+  const { pathname } = useLocation();
+  function saveMovieHandler() {
+    console.log("Фильм сохранен");
+  }
+  function deleteMovieHandler() {
+    console.log("Фильм удален");
+  }
+  return (
+    <li className="movie-card">
+      <img
+        className="movie-card__image"
+        alt={movieData.nameRu}
+        src={movieData.image}
+      />
+      <MovieCardButton
+        type={""}
+        onClickHandler={
+          pathname === "/movies" ? saveMovieHandler : deleteMovieHandler
+        }
+      >
+        {pathname === "/movies" ? "Сохранить" : "🞪"}
+      </MovieCardButton>
+      <div className="movie-card__description">
+        <p className="movie-card__name">{movieData.nameRU}</p>
+        <span className="movie-card__duration">
+          {convertingDuration(movieData.duration)}
+        </span>
+      </div>
+    </li>
+  );
 }
 
 export default MoviesCard;
