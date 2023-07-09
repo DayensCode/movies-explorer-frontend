40f4ad8956661188dc9a14c2924cbe486cc81323
Diana Nazarova <alllleksandrova@mail.ru>
2023-07-09 17:25:11 +0400

    feature: section not-found without adaptive

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,16 @@
+import './NotFound.css';
+import { useNavigate } from 'react-router-dom';
+
 function NotFound() {
+  const navigate = useNavigate();
   return (
-    <div>
-    </div>
+    <section className="not-found">
+      <h2 className="not-found__title">404</h2>
+      <p className="not-found__subtitle">Страница не найдена</p>
+      <button className="not-found__link" onClick={() => navigate(-1)}>
+        Назад
+      </button>
+    </section>
   )
 };
 
