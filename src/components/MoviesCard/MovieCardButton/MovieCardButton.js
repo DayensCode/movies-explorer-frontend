657bec0177aa7e40f13a6dce837cf82bbe28cda4
Diana Nazarova <alllleksandrova@mail.ru>
2023-07-14 12:19:04 +0400

    fix: syntax error in burger-button

--- src/components/MoviesCard/MovieCardButton/MovieCardButton.js
+++ src/components/MoviesCard/MovieCardButton/MovieCardButton.js
@@ -2,7 +2,7 @@ import './MovieCardButton.css';
 
 function MovieCardButton({children, type, onClickHandler}) {
 	return (
-		<button className={`movie-card-button ${type}`} type="button" onClick={onClickHandler}>
+		<button className={`movie-card-button ${type}`} onClick={onClickHandler}>
 			{children}
 		</button>
 	)
